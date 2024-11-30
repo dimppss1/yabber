fa3914d81fb23b0671025b425695fdd006404c34
dimppss1 <dimplesahoo01@gmail.com>
2024-11-30 20:35:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("celebrateButton").addEventListener("click", function() {
+    document.getElementById("confetti").classList.remove("hidden");
+    createConfetti();
+    document.getElementById("backgroundMusic").play();
+    this.classList.add("hidden");
+    document 
+    });
